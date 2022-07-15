0d0dcee686190bfedcf1a36ae9988195dd8d15d5
Omer Bokobza <104254718+Omer-Bokobza@users.noreply.github.com>
2022-07-15 14:44:44 +0300

    Delete Assignment_3.py

--- Assignment_3.py
+++
@@ -1,8 +0,0 @@
-lst = [31, 99, 3, 1943, 312, 12, 12]
-
-num = max(lst)
-
-max_list = [int(i) for i in str(num)]
-
-print("sort order - asc :", sorted(max_list))
-print("sort order - desc :", sorted(max_list, reverse=True))
